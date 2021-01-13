4a242aa7654c5b872c191a3a4031826cb4f53a36
Brayden Schnell <22schnellb@mydacc.org>
2021-01-13 08:16:21 -0500

    First commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 12, -10);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //offsets camera behind vehicle
+        transform.position = player.transform.position + offset;
+    }
+}
